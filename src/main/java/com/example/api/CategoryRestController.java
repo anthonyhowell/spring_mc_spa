81a33a27095a8a3c957a5529d762e634d185410a
Anthony Howell <anthonyhowell@gmail.com>
2016-07-19 21:45:20 -0400

    Base rest controller to provide root mapping adherence

--- src/main/java/com/example/api/CategoryRestController.java
+++ src/main/java/com/example/api/CategoryRestController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CategoryRestController {
-
+public class CategoryRestController extends BaseRestController {
 
     @Autowired
     private CategoryRepository repository;
--- src/main/java/com/example/api/PostRestController.java
+++ src/main/java/com/example/api/PostRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PostRestController {
+public class PostRestController extends BaseRestController {
 
     @Autowired
     private PostRepository repository;
